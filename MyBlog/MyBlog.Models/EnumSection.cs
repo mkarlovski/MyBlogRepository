d3d9ed65baefd6e8403267a62452a1092dbb04de
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-05-25 13:13:02 +0200

    update fullView

---
+++ MyBlog/MyBlog.Models/EnumSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.Models
+{
+    public enum EnumSection
+    {
+        Gadgets,
+        HowTo,
+        OS,
+        Mobile,
+        Apps
+    }
+}
